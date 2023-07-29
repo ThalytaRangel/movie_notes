8b59a144bbe720b4a051358ba6839ff8927230e7
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-07-28 22:03:19 -0300

    rating update

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,9 +1,14 @@
 const knex = require('../database/knex');
+const AppError = require('../utils/AppError');
 
 class NotesController {
   async create(request, response) {
     const {title, description, rating, tags} = request.body;
     const {user_id} = request.params;
+
+    if(rating < 1 || rating > 5) {
+    throw new AppError("Escolha um número de 1 a 5 para a avaliação do filme")
+    }
 
     const [note_id] = await knex("movie_notes").insert({
       title, 
@@ -72,7 +77,7 @@ class NotesController {
     }
 
     const userTags = await knex("movie_tags").where({user_id});
-    
+
     const notesWithTags = notes.map(note => {
       const noteTags = userTags.filter(tag => tag.note_id === note.id);
       return {
